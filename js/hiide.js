c6fa53fea8dd6b414d083c011f3e68cbbe6a2034
MadsDufour <mads825c@edu.easj.dk>
2019-09-17 12:24:10 +0200

    Plugin er færdiiiiig

--- js/hiide.js
+++ js/hiide.js
@@ -7,6 +7,10 @@
 		// Options
 		// Sætter default options, hvis der ikke sendes noget med
 		let defaultOptions = {
+			options:{
+				animation: 'fadeToggle',
+				animTime: 1500
+			}
 		
 		};
 		
@@ -14,6 +18,19 @@
 
 		// Går gennem alle elementer der matcher selector = idx=index, el=element
 		return this.each(function (idx, el) {
+
+		    $(".hideBtn").click(function () {
+		    	if (opts.options.animation == "fadeToggle") {
+		    		$(".hideDiv").fadeToggle(opts.options.animTime);
+		    	}
+		    	else if (opts.options.animation == 'slideToggle') {
+		    		$(".hideDiv").slideToggle(opts.options.animTime);
+                }
+
+                else {
+                	$(".hideDiv").hide(opts.options.animTime);
+                }
+            })
 			
 		});
 	};
